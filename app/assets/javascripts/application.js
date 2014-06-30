3841fa7f863099fca174e8c39a5efcf71fc3cdec
Jody Albritton <jody.albritton@gmail.com>
2014-06-30 18:20:36 -0500

    modified:   app/assets/font/FontAwesome.otf 	modified:   app/assets/font/fontawesome-webfont.eot 	modified:   app/assets/font/fontawesome-webfont.svg 	modified:   app/assets/font/fontawesome-webfont.ttf 	modified:   app/assets/font/fontawesome-webfont.woff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,20 +9,10 @@
 //
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
-//
 //= require jquery
 //= require jquery_ujs
-//= require modernizr.min
-//= require bootstrap
-//= require jquery.easing.1.3
-//= require jquery.appear
-//= require holder
-//= require slider_revolution
-//= require jquery.themepunch.plugins.min
-//= require jquery.themepunch.revolution
-//= require plugins/mediaelement/build/mediaelement-and-player.min.js
-//= require plugins/stellar/jquery.stellar.js
-//= require plugins/owl-carousel/owl.carousel.js
-//= require masonry.js
-//= require jquery.isotope.js
-//= require scripts
+//= require bootstrap.min
+//= require functions
+//= require responsiveCarousel
+//= require slimbox2
+//= require_self
