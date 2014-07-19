8ae60914dfaf76fb7a1d3c768db216ebfcb62035
Jody Albritton <jody.albritton@gmail.com>
2014-07-19 16:13:40 -0500

    fix taggings

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -10,7 +10,7 @@ class TagsController < ApplicationController
   	@tagged_videos = Video.tagged_with(@tag)
   	@tagged_posts = Post.tagged_with(@tag)
   	@tagged_galleries = Gallery.tagged_with(@tag)
-  	@tagged_sponsor = Sponsor.tagged_with(@tag)
+  	@tagged_sponsors = Sponsor.tagged_with(@tag)
   	add_breadcrumb  @tag.name, tag_url(@tag)
   end
 end
