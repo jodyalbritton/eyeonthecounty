914a89893ef47cf8ae3394d9a3fe955a1551dc4a
Jody Albritton <jody.albritton@gmail.com>
2014-07-19 01:48:21 -0500

    modified:   app/assets/stylesheets/application.css.scss 	modified:   app/assets/stylesheets/sponsors.css.scss 	modified:   app/controllers/sponsors_controller.rb 	modified:   app/views/layouts/application.html.erb 	modified:   app/views/pages/home.html.erb 	modified:   app/views/sponsors/show.html.erb

--- app/controllers/sponsors_controller.rb
+++ app/controllers/sponsors_controller.rb
@@ -1,10 +1,14 @@
 class SponsorsController < ApplicationController
+	add_breadcrumb "Home", :root_url
+ 	add_breadcrumb "Sponsors", :sponsors_url
   def index
   	@sponsors = Sponsor.all
   	@categories = Category.all
   end
 
   def show 
+
   	@sponsor = Sponsor.friendly.find(params[:id])
+  	add_breadcrumb @sponsor.name, sponsor_url(@sponsor)
   end
 end
