4a20fee1934d41512ceb258bf583b6279aaf0ee9
Jody Albritton <jody.albritton@gmail.com>
2014-07-19 16:11:34 -0500

    modified:   app/controllers/tags_controller.rb 	modified:   app/views/pages/home.html.erb 	modified:   app/views/tags/show.html.erb

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -6,6 +6,11 @@ class TagsController < ApplicationController
 
   def show
   	@tag = ActsAsTaggableOn::Tag.friendly.find(params[:id])
+
+  	@tagged_videos = Video.tagged_with(@tag)
+  	@tagged_posts = Post.tagged_with(@tag)
+  	@tagged_galleries = Gallery.tagged_with(@tag)
+  	@tagged_sponsor = Sponsor.tagged_with(@tag)
   	add_breadcrumb  @tag.name, tag_url(@tag)
   end
 end
