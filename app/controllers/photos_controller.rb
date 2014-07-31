3402150e80de12080e234d35074ad52621774797
Jody Albritton <jody.albritton@gmail.com>
2014-07-31 03:56:10 -0500

    fix photos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,11 +1,14 @@
 class PhotosController < ApplicationController
 	add_breadcrumb "Home", :root_url
-    add_breadcrumb "Blog", :blog_url
+  add_breadcrumb "Galleries", :galleries_url
+
 	impressionist :actions=>[:show]
 	before_action :set_photo, only: [:show]
 
 	def show
-		@comments = @photo.comment_threads.order(:cached_votes_up).reverse
+      add_breadcrumb @photo.gallery.name, gallery_url(@photo.gallery)
+      add_breadcrumb @photo.name
+		  @comments = @photo.comment_threads.order(:cached_votes_up).reverse
 	    @new_comment = Comment.build_from(@photo, current_user, "")
 	    @commentable = @photo
     end 
