c867b247b400646eb4eac18521c1ec79a81026c6
Jody Albritton <jody.albritton@gmail.com>
2014-07-31 03:30:47 -0500

    fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,7 +17,7 @@ module ApplicationHelper
       Video.where(:sponsored => true).first(3)
     end 
     def most_viewed
-      Video.where(:sponsored => false).order(:views).reverse.first(6)
+      Video.where(:sponsored => false).order(:impressions_count).reverse.first(6)
     end
     def featured_galleries
       Gallery.order(:created_at).first(6)
